77ed1c8f6239bd8b891fde9f7e70381c53e0869a
student <student>
2017-07-03 07:50:18 +0800

    barrier power const

--- Model/GameObject/model_const.py
+++ Model/GameObject/model_const.py
@@ -4,7 +4,9 @@ quaffleSpeed = 1
 goldenSnitchSpeed = 2
 playerInitPos = [[70,70],[70,670],[670,70],[670,670]]
 dirConst = [[0,0],[0,1],[0.707,-0.707],[1,0],[0.707,0.707],[0,-1],[-0.707,0.707],[-1,0],[-0.707,-0.707]]
-powerMax = 100
+powerMax = 1000
 freezeTime = 60
 numberOfQuaffles = 2
-playerBumpDistance = 225
+playerBumpDistance = 625
+
+barrierPowerCost = 180
