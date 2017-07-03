fa146053c565f40049fbf429c760fb335a006c94
student <student>
2017-07-03 15:50:27 +0800

    implement ball object's deprive function and update const

--- Model/GameObject/model_const.py
+++ Model/GameObject/model_const.py
@@ -3,6 +3,18 @@ ballRandomUpper = 470
 quaffleSpeed = 1
 goldenSnitchSpeed = 2
 player_init_pos = [[70,70],[70,670],[670,70],[670,670]]
-dir_const = [[0,0],[0,1],[0.707,-0.707],[1,0],[0.707,0.707],[0,-1],[-0.707,0.707],[-1,0],[-0.707,-0.707]]
+dirConst = [[0,0],[0,1],[0.707,-0.707],[1,0],[0.707,0.707],[0,-1],[-0.707,0.707],[-1,0],[-0.707,-0.707]]
 power_max = 100
 numberOfQuaffles = 2
+gameRange = 700
+gameRangeLower = 20
+gameRangeUpper = gameRangeLower + gameRange
+goalRange = 180
+goalRangeLower = gameRangeLower + (gameRange - goalRange) / 2
+goalRangeUpper = goalRangeLower + goalRange
+quaffleSize = 35
+goldenSnitchSize = 10
+
+#direction x, y bounce 
+dirBounce = [[0, 1, 8, 7, 6, 5, 4, 3, 2], [0, 5, 4, 3, 2, 1, 8, 7, 6]]
+
