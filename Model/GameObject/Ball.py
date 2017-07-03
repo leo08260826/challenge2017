f210d9884535ed0b21960cbfcb10a320466c2ed9
student <student>
2017-07-03 17:19:25 +0800

    implement getscore of successful shot and update const

--- Model/GameObject/Ball.py
+++ Model/GameObject/Ball.py
@@ -16,20 +16,60 @@ class OriginalBall(object):
         self.playerIndex = playerIndex
         self.state = 1
     
-    def deprive(self):
-        self.state = 0
-        self.isStrengthened = False
-    
     def throw(self, direction, isStrengthened = False):
         self.direction = direction
         self.isStrengthened = isStrengthened
         self.state = 2
+        self.speed = shotSpeed
         
     def tickCheck(self):
         if self.state in (0, 2):            
             self.position = [x + y * self.speed for x, y in \
                             zip(self.position, dirConst[self.direction])]
+            
+            tmpPosition = self.position
+            for index, element in enumerate(position):
+                if element < gameRangeLower:
+                    self.position[index] = gameRangeLower * 2 - element 
+                    self.direction = dirBounce[index][self.direction]
+                if element > gameRangeUpper:
+                    self.position[index] = gameRangeUpper * 2 - element
+                    self.direction = dirBounce[index][self.direction]
+            
+            return checkWhoseGoal(self, tmpPosition)
 
+    def checkWhoseGoal(self, position):
+        checkGoal = -1;
+        if position[0] < gameRangeLower:
+            if goalRangeLower < position[1] < goalRangeUpper:
+                checkGoal = 3
+            elif position[1] > goalRangeUpper or position[1] < goalRangeLower:
+                checkGoal = 4
+        elif position[0] > gameRangeUpper:
+            if goalRangeLower < position[1] < goalRangeUpper:
+                checkGoal = 1                
+            elif position[1] > goalRangeUpper or position[1] < goalRangeLower:
+                checkGoal = 4
+        elif position[1] < gameRangeLower:
+            if goalRangeLower < position[0] < goalRangeUpper:
+                checkGoal = 0
+            elif position[0] > goalRangeUpper or position[0] < goalRangeLower:
+                checkGoal = 4
+        elif position[1] > gameRangeLower:
+            if goalRangeLower < position[0] < goalRangeUpper:
+                checkGoal = 2
+            elif position[0] > goalRangeUpper or position[0] < goalRangeLower:
+                checkGoal = 4
+
+        if checkGoal in (-1, self.playerIndex):
+            return 0
+        elif checkGoal == 4:
+            return scoreOfQuaffles[5]
+        elif (checkGoal - self.playerIndex) in (-2, 2):
+            return scoreofQuaffles[4]
+        else:
+            return scoreofQuaffles[3]
+                
 class Quaffle(OriginalBall):
     def __init__(self, index):
         super(Quaffle, self).__init__(id, index)
@@ -37,7 +77,8 @@ class Quaffle(OriginalBall):
         self.ballsize = quaffleSize / 2
         
     def deprive(self, direction):
-        super(Quaffle, self).deprive(self)
+        self.state = 0
+        self.isStrengtheend = False
         self.direction = direction
 
     def tickCheck(self):
--- Model/GameObject/model_const.py
+++ Model/GameObject/model_const.py
@@ -1,22 +1,35 @@
 ballRandomLower = 270
 ballRandomUpper = 470
-quaffleSpeed = 1
-goldenSnitchSpeed = 2
+numberOfQuaffles = 2
+quaffleSize = 35
+scoreOfQuaffles = [1,0,4,8,12,16]
+
+quaffleSpeed = 3
+goldenSnitchSpeed = 8
+playerSpeed = 4
+
 playerInitPos = [[70,70],[70,670],[670,70],[670,670]]
-dirConst = [[0,0],[0,1],[0.707,-0.707],[1,0],[0.707,0.707],[0,-1],[-0.707,0.707],[-1,0],[-0.707,-0.707]]
 powerMax = 1000
 freezeTime = 60
-numberOfQuaffles = 2
+playerBumpDistance = 625
+barrierPowerCost = 180
+PlayerNum = 4
+
 gameRange = 700
 gameRangeLower = 20
 gameRangeUpper = gameRangeLower + gameRange
+
+
 goalRange = 180
 goalRangeLower = gameRangeLower + (gameRange - goalRange) / 2
 goalRangeUpper = goalRangeLower + goalRange
-quaffleSize = 35
+
 goldenSnitchSize = 10
+scoreOfGoldenSnitch = 32
 numberOfQuaffles = 2
-playerBumpDistance = 625
-barrierPowerCost = 180
+
+
 #direction x, y bounce 
+dirConst = [[0,0],[0,1],[0.707,-0.707],[1,0],[0.707,0.707],[0,-1],[-0.707,0.707],[-1,0],[-0.707,-0.707]]
 dirBounce = [[0, 1, 8, 7, 6, 5, 4, 3, 2], [0, 5, 4, 3, 2, 1, 8, 7, 6]]
+playerSpeed = 4
