8e3949c37bbe1e93f74d56b968a4b31aa2284fe2
b05902027 <peter9503@gmail.com>
2017-07-03 12:01:34 +0800

    add player function: set_barrier attack shot

--- Model/GameObject/Player.py
+++ Model/GameObject/Player.py
@@ -4,13 +4,19 @@ class player(object):
         self.name = name        
         self.index = index
 		self.position = player_init_pos[index]
-		self.direction = dir_const[0]
+		self.direction = 0
 		self.mode = 1
 		self.is_freeze = 0
 		self.power = 30
 		self.score = 0
 		self.skillcard = None
-		self.is_takeball = 0
+		self.takeball = -1
 		self.is_AI = is_AI
 		self.AI = AI
+
+	def set_barrier(self):
+
+	def attack(self):
+
+	def shot(self):
 
