9be375733a2e24731c7d61738fbdbf7fd62e0047
b05902027 <peter9503@gmail.com>
2017-07-03 11:43:07 +0800

    add player init and model_const

--- Model/GameObject/Player.py
+++ Model/GameObject/Player.py
@@ -1,5 +1,16 @@
+from model_const import *
 class player(object):
-    def __init__(self, name):
-        self.name = name
-        self.IS_AI = False
-        self.ai = None
+    def __init__(self, name, index, is_AI, AI = None):
+        self.name = name        
+        self.index = index
+		self.position = player_init_pos[index]
+		self.direction = dir_const[0]
+		self.mode = 1
+		self.is_freeze = 0
+		self.power = 30
+		self.score = 0
+		self.skillcard = None
+		self.is_takeball = 0
+		self.is_AI = is_AI
+		self.AI = AI
+
