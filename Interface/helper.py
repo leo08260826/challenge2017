db105c7de578ea42cfb3130e3809fc6c31ecc0d4
b05902059 <b05902059@linux7.csie.ntu.edu.tw>
2017-07-05 17:28:48 +0800

    mapInfo ballInfo

--- Interface/helper.py
+++ Interface/helper.py
@@ -5,51 +5,111 @@ class Helper(object):
     def __init__(self, model, index):
         self.model = model
         self.index = index
-    
+
+    def CountDist(Pos1,Pos2):
+        return (Pos2[0]-Pos1[0])**2+(Pos2[1]-Pos1[1])**2
+    def CountTan(Pos1,Pos2):
+        return tan(Pos2[1]-Pos1[1]/Pos2[0]-Pos1[0])
+
     # map info
     def getCaptureDir(self, pos):
-    	self.
+    	My_dir=[0,1,2,3,4,5,6,7,8]
+        tan=[100,2.414213,0.414213,-0.414213,-2.414213,2.414213,0.414213,-0.414213,-2.414213]
+        Pos1 = self.model.players[self.index].position
+        Pos2 = pos
+        tmpTan = CountTan(Pos1,Pos2)
+        if Pos2[0] >= Pos1[0] and Pos2[1] >= Pos1[1]:
+            if tmpTan <= tan[1] and tmpTan >= tan[2]:
+                return My_dir[2]
+            elif tmpTan > tan[1]:
+                return My_dir[1]
+            elif tmpTan < tan[2]:
+                return My_dir[3]
+        elif Pos2[0] >= Pos1[0] and Pos2[1] <= Pos1[1]:
+            if tmpTan >= tan[3]:
+                return My_dir[3]
+            elif tmpTan >= tan[4] and tmpTan <= tan[3]:
+                return My_dir[4]
+            elif tmpTan <= tan[4]:
+                return My_dir[5]
+        elif Pos2[0] <= Pos1[0] and Pos2[1] <= Pos1[1]:
+            if tmpTan >= tan[5]:
+                return My_dir[5]
+            elif tmpTan >= tan[6] and tmpTan <= tan[5]:
+                return My_dir[6]
+            elif tmpTan <= tan[6]:
+                return My_dir[7]
+        elif Pos2[0] <= Pos1[0] and Pos2[1] >= Pos1[1]:
+            if tmpTan >= tan[7]:
+                return My_dir[7]
+            elif tmpTan <= tan[7] and tmpTan >= tan[8]:
+                return My_dir[8]:
+            elif tmpTan <= tan[8]:
+                return My_dir[1]
 
     def getScoringDir(self, goal_id):
     	self.
     
     def getNearestGoal(self, pos):
-    	self.
-    
+    	board = [gameRangeLower,cornerGoalRangeLower,gateRangeLower,gateRangeUpper,cornerGoalRangeUpper,gameRangeUpper]
+        gate = [((board[2]+board[3])/2,board[0]),(board[5],(board[2]+board[3])/2),((board[2]+board[3])/2,board[5]),\
+                 (board[0],(board[2]+board[3])/2),((board[4]+board[5])/2,(board[0]+board[1])/2),((board[4]+board[5])/2,(board[4]+board[5])/2),\
+                 ((board[0]+board[1])/2,(board[4]+board[5])/2),((board[0],board[1])/2,(board[0]+board[1])/2)]
+        MinIndex = 0
+        Mini = 99999999
+        for i in range(8):
+            tmp = [gate[i][0],gate[i][1]]
+            if (CountDist(tmp,pos) < Mini):
+                Mini = CountDist(tmp,pos)
+                MinIndex = i
+        return MinIndex
+        
     def getTimeLeft(self):
-    	self.
-    
-    def isBall(self, pos):
-    	self.
-    
-    def isWall(self, pos):
-    	self.
-    
-    def isPlayer(self, pos):
-    	self.
-    
-    def isBarrier(self, pos):
-    	self.
-    
+    	return self.model.timer
+     
     # ball info
     def getFreeBallPos(self):
-    	self.
+    	Pos_list=[]
+        for i in range(numberOfQuaffles):
+            if self.model.quaffles[i].state == 0:
+                Pos_list.append(self.model.quaffles[i].position)
+        return Pos_list
 
     def getHoldBallPos(self):
-    	self.
-    	
+    	Pos_list=[]
+        for i in range(numberOfQuaffles):
+            if self.model.quaffles[i].state == 1:
+                Pos_list.append(self.model.players[self.model.quaffles[i].playerIndex].position)
+        return Pos_list
+
     def getFlyBallPos(self):
-    	self.
-    	
+    	Pos_list=[]
+        for i in range(numberOfQuaffles):
+            if self.model.quaffles[i].state == 2:
+                Pos_list.append(self.model.quaffles[i].position)
+    	return Pos_list
+
     def getGoldBallPos(self):
-    	self.
+    	return self.goldenSnitch.position
     	
-    def getNearBall(self):
-    	self.
-    	
-    def checkBallState(self, pos):
-    	self.
-    	
+    def getNearBallInfo(self):
+        Info_list=[]
+        for i in range(numberOfQuaffles):
+            tmp = []
+            tmp[0] = self.model.quaffles[i].position[0]
+            tmp[1] = self.model.quaffles[i].position[1]
+            tmp[2] = self.model.quaffles[i].state
+            tmpDist = 99999999
+            if tmp[2] == 1:
+                tmpDist = CountDist(self.model.players[self.model.quaffles[i].playerIndex].position,self.model.players[self.index].position)
+            elif tmp[2] == 0 or tmp[2] == 2:
+                tmpDist = CountDist(self.model.quaffles[i].position,self.model.players[self.index].position)
+            tmp[3] = tmpDist
+        tmp2 = [self.model.goldenSnitch.position[0],self.model.goldenSnitch.position[1],4,CountDist(self.model.goldenSnitch.position,self.model.players[self.index])]
+        Info_list.append(tmp2)
+        Sort_Info = sorted(Info_list,key=itemgetter(3))
+        return Sort_Info 
+
     # my info
     def getMyIndex(self):
     	return
@@ -133,7 +193,8 @@ class Helper(object):
 
     #skillcard
     def SkillHide(self):
-
+        self.model.evManager.Post(Event_SkillCard(self.index,0))
     def dkillDementor(slef):
-
+        self.model.evManager.Post(Event_SkillCard(self.index,1))
     def skillStunAll(self):
+        self.model.evManager.Post(Event_SkillCard(self.index,2))
