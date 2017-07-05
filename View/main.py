5d373bc92bbf0cacbc6731c1e49f05f1753276b4
oopzzozzo <ek3ru8m4@gmail.com>
2017-07-05 11:57:57 +0800

    checking

--- View/main.py
+++ View/main.py
@@ -86,10 +86,6 @@ class GraphicalView(object):
         for i in range(PlayerNum):
             self.render_player_character(i)
             
-        #for testing
-        for i in range(4):
-            self.render_player_status(i)
-        #end test
         for i in range(numberOfQuaffles):
             self.render_quaffle(i)
         self.render_goldenSnitch()
@@ -148,14 +144,13 @@ class GraphicalView(object):
         self.map_gray = pg.image.load('View/image/background/map_grayscale.png')
         self.time = pg.image.load('View/image/background/time.png')
         self.background = pg.image.load('View/image/background/backgroundfill.png')
-        for i in range(4):
-            self.playerInfo[i] = pg.image.load('View/image/background/info'+str(i+1)+'.png')
+        self.playerInfo = [ pg.image.load('View/image/background/info'+str(i+1)+'.png') for i in range(PlayerNum) ]
         ''' icons '''
         self.mode_images = [ pg.image.load('View/image/icon/icon_attack.png'),
                             pg.image.load('View/image/icon/icon_protectmode.png')]
         self.player_status0 = pg.image.load('View/image/icon/dizzyOninfo.png')
         self.player_status1 = pg.image.load('View/image/icon/shieldOninfo.png')
-        self.player_status2 = pg.image.load('View/image/icon/shadowOningo.png')
+        self.player_status2 = pg.image.load('View/image/icon/shadowOninfo.png')
         self.player_status_A = pg.image.load('View/image/icon/attackmodeOninfo.png')
         self.player_status_P = pg.image.load('View/image/icon/protectmodeOninfo.png')
         ''' skills '''
@@ -239,7 +234,7 @@ class GraphicalView(object):
                 self.blit_at_center(self.ball_normal_images[index], quaffle.position)
 
     def render_goldenSnitch(self):
-        if self.mode.goldenSnitch.stare != 1:
+        if self.model.goldenSnitch.state != 1:
             self.blit_at_center(self.goldenSnitch_images[pg.time.get_ticks() % 2], self.model.goldenSnitch.position)
 
     def render_barrier(self,barrier):
