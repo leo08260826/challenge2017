c07369c3233d6cd5f0a73c8b9b881ac16db6c8ee
empennage98 <williamlin1998@openmailbox.org>
2017-07-05 11:34:40 +0800

    Fix a syntax error in `main.py`

--- Model/main.py
+++ Model/main.py
@@ -162,8 +162,8 @@ class GameEngine(object):
         for barrier in self.barriers:
             for player in self.players:
                 if not barrier.playerIndex == player.index and barrier.bump(player):
-                    player.position[0] -= dirConst[player.direction][0]*playerSpeed
-                    player.position[1] -= dirConst[player.direction][1]*playerSpeed
+                    player.position[0] -= dirConst[player.direction][0]*playerSpeed[player.mode]
+                    player.position[1] -= dirConst[player.direction][1]*playerSpeed[player.mode]
         # barrier to quaffle
         for barrier in self.barriers:
             for quaffle in self.quaffles:
