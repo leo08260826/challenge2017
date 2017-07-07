9f5019b48184c708ee3a352015429a337380ed09
oopzzozzo <ek3ru8m4@gmail.com>
2017-07-07 15:00:21 +0800

    1 pixel

--- View/main.py
+++ View/main.py
@@ -181,7 +181,7 @@ class GraphicalView(object):
             lastrank = rank
             height = score / maxscore * 400
             color = Player_Colors[ranked[i].index]
-            pg.draw.rect(self.screen, Color_White, (260+200*i, 600-height, 120, height))
+            pg.draw.rect(self.screen, Color_White, (260+200*i, 600-height, 120, height+1))
             self.screen.blit(self.pennant_images[ranked[i].index], (260+200*i,610-height))
             self.screen.blit(self.player_photo[ranked[i].index], (260+200*i,600))
             pg.draw.rect(self.screen, Color_White, (260+200*i, 600-height, 120, 70))
