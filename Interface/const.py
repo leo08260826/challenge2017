dbefa52d72cde150fc8c1a48b558bcb9bb74702b
曾千育 <Melody@cengqianyude-MacBook-Pro.local>
2017-07-09 22:09:42 +0800

    modify parameter

--- Interface/const.py
+++ Interface/const.py
@@ -7,14 +7,15 @@ AI_LD = 6
 AI_L  = 7
 AI_LU = 8
 
-AI_ACTION_1 = 9 # power throw / barrier
-AI_ACTION_2 = 10 # stun / mask
-AI_THROW = 11 # general throw
+AI_ATTACK_POWERTHROW = 9
+AI_ATTACK_STUN = 10
+AI_DEFEND_BARRIER = 9
+AI_DEFEND_MASK = 10
 
+AI_THROWBALL = 11
 AI_MODECHANGE = 12
 AI_SKILLCARD_HIDE = 13 # invisible
 AI_SKILLCARD_DEMENTOR = 14 # empty power
 AI_SKILLCARD_STUNALL = 15 # stun all enermy
-
-AI_CallMe = 16
-AI_NoUseSkillCard = 17
+AI_SKILLCARD_SPECIAL = 16
+AI_CALLME = 17
